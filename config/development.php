4689e25ada981a891914a47dea34aab0b01e4982
Michael Shapiro <michaels@sinapsa.co.il>
2013-02-26 13:46:11 +0200

    Added CSS & JS settings definitions

--- config/development.php
+++ config/development.php
@@ -4,5 +4,16 @@
 	// Development enviroment settings
 	// 
 
+
 	// Base settings
 	define('BASE_URI', '/Projects/Sinapsa/Phpngo/');
+
+
+	// Media
+	define('CSS_CONCAT', false);
+	define('CSS_COMPRESS', false);
+	define('CSS_CACHE_DIR', PH_PATH . CSS_URL . 'cache/');
+
+	define('JS_CONCAT', false);
+	define('JS_COMPRESS', false);
+	define('JS_CACHE_DIR', PH_PATH . JS_URL . 'cache/');
